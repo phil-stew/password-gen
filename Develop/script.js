7d4b60bfe49395ef2d5f49e291f45a2a23860a93
Philly <Phil.stew@live.ca>
2021-03-09 01:41:39 -0500

    workking code

--- Develop/script.js
+++ Develop/script.js
@@ -6,62 +6,143 @@ function writePassword() {
   
   var passLength = prompt("How long do you want you password")
   
-  if (passLength < 8 && passLength > 129 && passLength === String){
-      alert("try again")  
-      return
+  if (passLength < 8 || passLength > 129 || passLength == false ){
+      alert("Please try again");  
+      return;
   }
    
    else{
-    var aphla = confirm("Do you want Letter")
+    var aphla = confirm("Do you want lowercase letters")
+    var aphlaHigh = confirm("Do you want uppercase letters")
     var num = confirm(" Do you want numbers")
     var specialC = confirm("Do you what special characters" )
    };
 
-
-   if (aphla == true && num == true && specialC == true){
-    alert("num%spc")
-     } 
+   if (aphla == true && num == true && specialC == true && aphlaHigh == true){
+    function generatePassword( len ) {
+      var length = (len)?(len):(passLength);
+      var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+      var numeric = '0123456789';
+      var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+      var password = "";
+      var character = "";
+      var crunch = true;
+      while( password.length<length ) {
+          entity1 = Math.ceil(string.length * Math.random()*Math.random());
+          entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+          entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+          hold = string.charAt( entity1 );
+          hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+          character += hold;
+          character += numeric.charAt( entity2 );
+          character += punctuation.charAt( entity3 );
+          password = character;
+      }
+      password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+      return password.substr(0,len);
+  // found the function on stackoverflow https://stackoverflow.com/questions/1497481/javascript-password-generator
+   }} 
  
-   else if(aphla == false && num == true && specialC == true){
-    function generatePassword(length) {
-      lenght = passLength
-      var result           = '';
-      var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var charactersLength = characters.length;
-      for ( var i = 0; i < length; i++ ) {
-         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   else if(aphla == false && num == true && specialC == true && aphlaHigh == true){
+    function generatePassword( len ) {
+      var length = (len)?(len):(passLength);
+      var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+      var numeric = '0123456789';
+      var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+      var password = "";
+      var character = "";
+      var crunch = true;
+      while( password.length<length ) {
+          entity1 = Math.ceil(string.length * Math.random()*Math.random());
+          entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+          entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+          hold = string.charAt( entity1 );
+          hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+          character += hold;
+          character += numeric.charAt( entity2 );
+          character += punctuation.charAt( entity3 );
+          password = character;
       }
-      return result;
-   }
-   
+      password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+      return password.substr(0,len);
+   }}
 
-  
-   console.log(password)
-  console.log(passLength)
-  console.log(aphla)
-  console.log(generatePassword(passLength))
-
-    
-   
+   else if(aphla == true && num == false && specialC == true && aphlaHigh == true){
+    function generatePassword( len ) {
+      var length = (len)?(len):(passLength);
+      var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+      var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+      var password = "";
+      var character = "";
+      var crunch = true;
+      while( password.length<length ) {
+          entity1 = Math.ceil(string.length * Math.random()*Math.random());
+          entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+          hold = string.charAt( entity1 );
+          hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+          character += hold;
+          character += punctuation.charAt( entity3 );
+          password = character;
       }
+      password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+      return password.substr(0,len);
+   }}
 
+   else if(aphla == true && num == true && specialC == false &&aphlaHigh == true){
+    function generatePassword( len ) {
+      var length = (len)?(len):(passLength);
+      var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+      var numeric = '0123456789';
+      var password = "";
+      var character = "";
+      var crunch = true;
+      while( password.length<length ) {
+          entity1 = Math.ceil(string.length * Math.random()*Math.random());
+          entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+          hold = string.charAt( entity1 );
+          hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+          character += hold;
+          character += numeric.charAt( entity2 );
+          password = character;
+      }
+      password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+      return password.substr(0,len);
+   }}
 
-   else if(aphla == true && num == false && specialC == true){
-    alert("apha%spc")
-   }
-   else if(aphla == true && num == true && specialC == false){
-    alert("num%salpha")
-   }
+   if (aphla == true && num == true && specialC == true && aphlaHigh == false){
+    function generatePassword( len ) {
+      var length = (len)?(len):(passLength);
+      var string = "abcdefghijklmnopqrstuvwxyz"; 
+      var numeric = '0123456789';
+      var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+      var password = "";
+      var character = "";
+      var crunch = true;
+      while( password.length<length ) {
+          entity1 = Math.ceil(string.length * Math.random()*Math.random());
+          entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+          entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+          hold = string.charAt( entity1 );
+          //hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+          character += hold;
+          character += numeric.charAt( entity2 );
+          character += punctuation.charAt( entity3 );
+          password = character;
+      }
+      password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+      return password.substr(0,len); 
+    }}
+   
    else{
-     alert(" You need one number and letters or special character!!")
+     alert(" You need to choose three of four options!!")
    }
 
 
-var password = generatePassword(passLength)
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-  
+     var password = generatePassword(passLength)
+     var passwordText = document.querySelector("#password");
+     passwordText.value = password;
+     
+  }
 
 
 
@@ -72,7 +153,7 @@ var password = generatePassword(passLength)
 
 
   
-}
+
 
 
 // Add event listener to generate button
